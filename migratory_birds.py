3911c50757c18d74b3bb188219d9992ad6b93c27
Denis Junuzovic <denisj1688@gmail.com>
2020-11-09 19:16:43 +0100

    working solution with memoization

--- migratory_birds.py
+++ migratory_birds.py
@@ -1,20 +1,22 @@
+import operator
+
 def migratoryBirds(arr):
     countDict = {}
-
+ 
     for n in arr:
+        if n in countDict:
+            continue
         countDict.update({n: arr.count(n)})
 
-    sortedCountArr = sorted(countDict.items(), key=lambda x: x[1], reverse=False)
+    sortedCountArr = sorted(countDict.items(), key=operator.itemgetter(1))
 
-    size = len(sortedCountArr)
+    if sortedCountArr[-1][1] != sortedCountArr[-2][1]:
+        return sortedCountArr[-1][0]
 
-    if sortedCountArr[size-1][1] != sortedCountArr[size-2][1]:
-        return sortedCountArr[size-1][0]
-
-    if sortedCountArr[size-1][0] > sortedCountArr[size-2][0]:
-        return sortedCountArr[size-2][0]
+    if sortedCountArr[-1][0] > sortedCountArr[-2][0]:
+        return sortedCountArr[-2][0]
     
-    return  sortedCountArr[size-1][0]
+    return  sortedCountArr[-1][0]
 
 
 print(migratoryBirds([1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]))
